ef43afc1cec03807960f4cb25fc056d93b606d45
Mostafa <m.torbon@gmx.de>
2020-10-10 09:08:07 +0200

    Readded configfile for tomcatdeploy

--- vars/tomcatdeploy.groovy
+++ vars/tomcatdeploy.groovy
@@ -1,7 +1,7 @@
 def call() {
     //withCredentials([usernamePassword(credentialsId: 'tomcat', usernameVariable: 'TOMCAT_USER', passwordVariable: 'TOMCAT_PASSWORD')]) {
-        //configFileProvider([configFile(fileId: 'f3ef7a41-a468-41f2-8819-7a02ecf6050b', variable: 'MAVEN_GLOBAL_SETTINGS')]) {
+        configFileProvider([configFile(fileId: 'f3ef7a41-a468-41f2-8819-7a02ecf6050b', variable: 'MAVEN_GLOBAL_SETTINGS')]) {
         sh 'mvn -gs $MAVEN_GLOBAL_SETTINGS tomcat7:deploy -e -DskipTests -DdeployOnly'
         //}
-    //}
+    }
 }
